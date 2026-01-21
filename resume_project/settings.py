ab4442cc2f63ca5c1a0819c7f032658e4c793542
huzaifalimbadiya <hujjuhujju729@gmail.com>
2026-01-21 23:17:34 +0530

    Use SQLite for Render deployment

--- resume_project/settings.py
+++ resume_project/settings.py
@@ -82,18 +82,11 @@ WSGI_APPLICATION = 'resume_project.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-
-if not os.environ.get('DATABASE_URL'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
